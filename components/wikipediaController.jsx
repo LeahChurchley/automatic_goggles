1ace5b399e7e0da1de58c92448fda736cfffa39a
Ross Churchley <ross@rosschurchley.com>
2016-07-22 17:11:43 -0700

    Fix and simplify Wikipedia controller

--- components/wikipediaController.jsx
+++ components/wikipediaController.jsx
@@ -1,43 +1,45 @@
 'use strict';
+
 import React from 'react';
-import $ from "jquery";
-import Wikipedia from './wikipedia.jsx';
+import $ from 'jquery';
+
+
+/* WikipediaController
+ *
+ * A widget that presents the title and first paragraph of a Wikipedia article
+ * matching a given search term.
+ *
+ */
 
 export default class WikipediaController extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {data:[],loading: true};
-  }
-  componentWillMount(){
-      $.ajax({
-      type: 'POST',
-      data: {search_term: this.props.data},
-      url: '/wikipedia',
-      success: function(wikiData){
-        this.setState({data: wikiData, loading:false});
-      }.bind(this)
-    });
-  }
-  componentWillReceiveProps(){
-    this.setState({data:[],loading:true});
-    $.ajax({
-    type: 'POST',
-    url: '/wikipedia',
-    data: {search_term: this.props.data},
-    dataType: 'json',
-    success: function(wikiData){
-      this.setState({data: wikiData, loading:false});
-    }.bind(this),
-    error: function(err) { console.error('error', err) }.bind(this)
-    });
-  }
+
   render(){
     return(
-        <div>
-          {!this.state.loading ?
-            <Wikipedia title={this.state.data.data.title} body={this.state.data.data.body} facts={this.state.data.data.facts} />:
-              <img className="loading" src="images/loading_spinner.gif" alt="Loading..." />}
-       </div>
-     )
+      <div>
+        <h2>{this.state.data.title}</h2>
+        <p>{this.state.data.body}</p>
+      </div>
+    )
   }
+
+  constructor(props){
+    super(props);
+    this.state = {data: {}, loading: false};
+  }
+
+  componentWillReceiveProps(newProps){
+    this.setState({data: {}, loading: true});
+    $.ajax({
+      type: 'POST',
+      url: '/api/wikipedia',
+      data: {search_term: newProps.term},
+      dataType: 'json',
+      success: function(wikiData){
+        this.setState({data: wikiData, loading:false});
+      }.bind(this),
+      error: function(err) { console.error('error', err) }.bind(this)
+    });
+  }
+
+
 }
