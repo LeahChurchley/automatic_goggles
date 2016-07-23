cb232cc9fc4e7d137f7e898994021f48c3e04c27
Ross Churchley <ross@rosschurchley.com>
2016-07-22 17:12:16 -0700

    Fix and simplify Twitter controller

--- components/twitterController.jsx
+++ components/twitterController.jsx
@@ -1,74 +1,46 @@
 'use strict';
+
 import React from 'react';
-import $ from "jquery";
+import $ from 'jquery';
+
+
+/* Twitter
+ *
+ * A widget that lists popular tweets with a given hashtag
+ *
+ */
 
 export default class TwitterController extends React.Component {
+
+  render(){
+    // Loop over all tweets in this.state.data and render an li for each
+    return(
+      <ul>
+        {this.state.data.map(function(tweet, i) {
+          return <li dangerouslySetInnerHTML={{__html: tweet.html }} key={i} />;
+        })}
+      </ul>
+    )
+  }
+
   constructor(props){
     super(props);
-    this.state = {data:[],loading: true, index: 0};
+    this.state = {data: [], loading: false};
   }
-  componentDidMount(){
-      this.serverRequest =$.ajax({
+
+  componentWillReceiveProps(newProps){
+    this.setState({data: [], loading: true});
+    $.ajax({
       type: 'POST',
-      url: '/twitter',
-      data: {search_term: this.props.data},
+      url: '/api/twitter',
+      data: {search_term: newProps.term},
       dataType: 'json',
       success: function(tweets){
-        this.setState({data: tweets, loading:false});
+        this.setState({data: tweets, loading: false});
       }.bind(this),
       error: function(err) { console.error('error', err) }.bind(this)
     });
   }
-  componentWillReceiveProps(){
-    clearTimeout(this.myvar);
-    this.setState({data:[],loading:true});
-    $.ajax({
-    type: 'POST',
-    url: '/twitter',
-    data: {search_term: this.props.data},
-    dataType: 'json',
-    success: function(tweets){
-      this.setState({data: tweets, loading:false});
-    }.bind(this),
-    error: function(err) { console.error('error', err) }.bind(this)
-    });
-  }
-  componentDidUpdate(){
-    if(twttr){
-      window.twttr.widgets.load(document.getElementsByClassName("MNLAtwitter"));
-    }
-  }
-  render(){
-    if(!this.state.loading){
-    var interval = setTimeout(()=>{
-      // setTimeout
-      if (this.state.index >= this.state.data.data.length-1){
-        this.setState({index: 0});
-      }else{
-        this.setState({index: this.state.index + 4});
-      }
-    },10000);}
-    return(
-      <div>
-      {!this.state.loading ?
-        <ul>
-          <li>
-            <div className="MNLAtwitter" dangerouslySetInnerHTML={{__html: this.state.data.data[this.state.index].html }} />
-          </li>
-          <li>
-            <div  className="MNLAtwitter" dangerouslySetInnerHTML={{__html: this.state.data.data[this.state.index + 1].html }} />
-          </li>
-          <li>
-            <div className="MNLAtwitter" dangerouslySetInnerHTML={{__html: this.state.data.data[this.state.index + 2].html }} />
-          </li>
-          <li>
-            <div className="MNLAtwitter" dangerouslySetInnerHTML={{__html: this.state.data.data[this.state.index + 3].html }} />
-          </li>
-        </ul>:
-        <img className="loading" src="images/loading_spinner_inverse.gif" alt="Loading..." />}
-      </div>
-    )};
+
 }
 
-{/*<h1>
- </h1>*/}
